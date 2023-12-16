7fe21e31d40a3ea9c8bfd3b9e7bf0d5ec3780e37
Lucas Duarte <lucasduartesouza@hotmail.com>
2023-12-16 17:44:29 -0300

    minor adjusts

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ $(document).ready(() => {
 	let win = new Audio('win.mp3');
 	let jump = new Audio('jump.mp3');
 	let hasPlayed = false;
+	let isWin = false;
 
 	$(window).click(function () {
 		$("li").removeClass("active");
 	});
 
 	$(".goal").on("click", (e) => {
+		isWin = true;
 		ost.pause();
 		win.play();
 	});
 
 	$("li a").on("click", (e) => {
-		jump.play();
 		if (!hasPlayed) {
 			ost.play();
 			hasPlayed = true;
@@ -26,9 +27,13 @@ $(document).ready(() => {
 			block: "start",
 			inline: "center"
 		});
+
+		if (!isWin) {
+			jump.play();
+		}
+
 		setTimeout(() => {
 			$(".mario").addClass("jump");
-		
 		}, 200);
 		setTimeout(() => {
 			$(".mario").removeClass("jump");
